7fc725a9c81f69b60e5035bed0a9dbfffcd983fc
Yadd <yadd@debian.org>
2023-07-19 14:03:38 +0400

    Build a standalone bin

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+// rollup -i server.js -o test.js -f cjs -p @rollup/plugin-node-resolve -p @rollup/plugin-commonjs -p @rollup/plugin-json -p @rollup/plugin-terser
+
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import terser from '@rollup/plugin-terser'
+import hashbang from 'rollup-plugin-shebang-bin'
+
+export default {
+  input: 'src/server.js',
+  output: {
+    format: 'cjs',
+    dir: 'dist',
+    entryFileNames: '[name].js',
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    json(),
+    terser(),
+    hashbang(),
+  ],
+};
